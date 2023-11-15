512d6e114bbbe2069a63daefb2a7eb74a3942395
SaadAkram200 <saadakram200@gmail.com>
2023-11-15 10:10:13 +0500

    added splash screen using flutter_native_splash liberary

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,14 +1,24 @@
+// ignore: unused_import
 import 'package:first_app/App/others_location.dart';
+// ignore: unused_import
 import 'package:first_app/Google%20Map/google_maps.dart';
+// ignore: unused_import
 import 'package:first_app/Google%20Map/map_firebase.dart';
+// ignore: unused_import
 import 'package:first_app/Google%20Map/markerAt_center.dart';
+// ignore: unused_import
 import 'package:first_app/App/home_screen.dart';
+// ignore: unused_import
 import 'package:first_app/App/signin_screen.dart';
 import 'package:first_app/App/signup_screen.dart';
 import 'package:first_app/firebase_options.dart';
+// ignore: unused_import
 import 'package:first_app/API%20work/login_screen.dart';
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:first_app/API%20work/get_data.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+// ignore: unused_import
 import 'API work/upload_image.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -17,6 +27,8 @@ void main()async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
 
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
@@ -27,6 +39,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
